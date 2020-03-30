0e91a6602030ede8151db3f7d44b25aa4d6d1fbe
rickfurt <ricardofurtado2609@gmail.com>
2020-03-30 19:48:43 +1100

    chore(ui): created headbar on landing page

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { FiLogIn } from "react-icons/fi";
+import React from "react";
+import { Link } from "react-router-dom";
 
 import "./styles.css";
 
@@ -10,19 +9,36 @@ import logoImg from "../../assets/logo.svg";
 export default class Home extends React.Component {
     render() {
       return <>
-        <div className="profile-container">
+        <div className="home-container">
             <header>
-                <img src={logoImg} alt="Be the Hero" />
-                <div className="user-btn">
-                    <Link className="button" to="/login">
+                <img className="logo-home" src={logoImg} alt="Be the Hero" />
+                <div className="user-container">
+                    <Link className="button user-btn" to="/login">
                     Login
                     </Link>
-                    <Link className="button" to="/register">
+                    <Link className="button user-btn" to="/register">
                     Signup
                     </Link>
                 </div>
             </header>    
         </div>
+        <div className="banner-container">
+        <div className="banner-text">
+        <h1 className="text-title">About Us</h1>
+        <p>We're a non profit platform created in order to help those in need.
+        This platform goal is to address all the needs in only one place to be easier to help or get help.</p>
+        <br></br>
+        <h1 className="text-title">How it works?</h1>
+        <ol>
+        <li>Those in need are encouraged to signup and enter their needs at register new case Section, also will be requested some personal information.</li>
+        <li>Once the previous step is completed the platform will share to and encourage the rest of the community to check all those in need and how to help.</li>
+        </ol>
+</div>
+        <div className="banner-img">
+            <img className="banner-home" src={heroesImg} alt="banner" />    
+        </div>
+        </div>
+
     </>
     }
   }
